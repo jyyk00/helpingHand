c136edd264d4a86b9b2fc1aa4aa3b0e5bf31463d
programmerkim <jenniferkim.businessonly@gmail.com>
2020-03-10 00:40:25 -0700

    cache file

---
+++ public/service-worker.js
@@ -0,0 +1,9 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/index.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+  ];

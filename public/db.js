d2069d4919de11845fbf8b44870e8ed070559a7a
programmerkim <jenniferkim.businessonly@gmail.com>
2020-03-10 01:02:45 -0700

    db final

--- public/db.js
+++ public/db.js
@@ -52,3 +52,18 @@ request.onupgradeneeded = function(event) {
         })
         .then(response => response.json())
         .then(() => {
+            // if successful, open a transaction on your pending db
+        const transaction = db.transaction(["pending"], "readwrite");
+
+        // access your pending object store
+        const store = transaction.objectStore("pending");
+
+        // clear all items in your store
+        store.clear();
+      });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);

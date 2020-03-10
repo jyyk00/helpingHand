4033dcea6441ef1bcd01220e426ca7d2fa4e2033
programmerkim <jenniferkim.businessonly@gmail.com>
2020-03-10 01:12:20 -0700

    event lis

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,6 +11,49 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v2";
 
+// install
+self.addEventListener("install", function(event) {
+    event.waitUntil(caches.open(CACHE_NAME).then( cache => {
+        return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
 
+// activate
+self.addEventListener("activate", function(event) {
+    event.waitUntil(caches.keys().then(keyList => {
+        return Promise.all( 
+            keyList.map( key => {
+            if ( key !== CACHE_NAME && key!== DATA_CACHE_NAME ) {
+                console.log('Removing old key: ', key);
+                return caches.delete(key);
+            };
+        }));
+    }));
+    self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+      return;
+    }
 
 
